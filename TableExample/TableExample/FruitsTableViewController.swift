cc75cd70647adde234dd145520a2b6fee57ba091
san2ride <sanchezjason@me.com>
2016-10-31 11:18:08 -0600

    iOS 10 Custom tableView

---
+++ TableExample/TableExample/FruitsTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FruitsTableViewController.swift
+//  TableExample
+//
+//  Created by don't touch me on 10/31/16.
+//  Copyright © 2016 trvl, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FruitsTableViewController: UITableViewController {
+
+    var fruits = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return fruits.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+
+        let fruitName = fruits[indexPath.row]
+        cell.textLabel?.text = fruitName
+        cell.detailTextLabel?.text = "Delicious!"
+        cell.imageView?.image = UIImage(named: fruitName)
+        
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        
+//        return "Section \(section)"
+//    }
+    
+
+
+}
